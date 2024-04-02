be026750ee07677ad94086a5c353b8e8c613585e
Tanmay451 <tanmaypiyushkaushik451@gmail.com>
2024-04-02 21:04:49 +0530

    handle multiple requests in parallel

--- tcp/server.go
+++ tcp/server.go
@@ -3,6 +3,7 @@ package tcp
 import (
 	"fmt"
 	"net"
+	"time"
 )
 
 func Run() {
@@ -10,13 +11,14 @@ func Run() {
 
 	for {
 		conn, _ := l.Accept()
+		go func(conn net.Conn) {
+			buf := make([]byte, 1024)
+			conn.Read(buf)
+			fmt.Println(string(buf))
 
-		buf := make([]byte, 1024)
-		conn.Read(buf)
-		fmt.Println(string(buf))
-
-		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nHello, World!\r\n"))
-
-		conn.Close()
+			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nHello, World!\r\n"))
+			time.Sleep(5 * time.Second)
+			conn.Close()
+		}(conn)
 	}
 }
