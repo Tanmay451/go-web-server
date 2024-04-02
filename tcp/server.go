e45cab4d16968666d525bad32da196c0745d3a05
Tanmay451 <tanmaypiyushkaushik451@gmail.com>
2024-04-02 20:37:47 +0530

    keep server alive to handle multiple request one by one

--- tcp/server.go
+++ tcp/server.go
@@ -8,13 +8,15 @@ import (
 func Run() {
 	l, _ := net.Listen("tcp", ":8080")
 
-	conn, _ := l.Accept()
+	for {
+		conn, _ := l.Accept()
 
-	buf := make([]byte, 1024)
-	conn.Read(buf)
-	fmt.Println(string(buf))
+		buf := make([]byte, 1024)
+		conn.Read(buf)
+		fmt.Println(string(buf))
 
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nHello, World!\r\n"))
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nHello, World!\r\n"))
 
-	conn.Close()
+		conn.Close()
+	}
 }
