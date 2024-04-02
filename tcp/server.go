c492d2711e6848401a27cd77d019cc248b6c9b94
Tanmay451 <tanmaypiyushkaushik451@gmail.com>
2024-04-02 20:33:01 +0530

    add read/write in connection

--- tcp/server.go
+++ tcp/server.go
@@ -1,9 +1,20 @@
 package tcp
 
-import "net"
+import (
+	"fmt"
+	"net"
+)
 
 func Run() {
 	l, _ := net.Listen("tcp", ":8080")
+
 	conn, _ := l.Accept()
+
+	buf := make([]byte, 1024)
+	conn.Read(buf)
+	fmt.Println(string(buf))
+
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nHello, World!\r\n"))
+
 	conn.Close()
 }
