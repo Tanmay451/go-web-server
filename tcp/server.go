014a0120bf8dd2dec82ed32b7def32925c7baaf4
Tanmay451 <tanmaypiyushkaushik451@gmail.com>
2024-04-02 21:15:06 +0530

    add deadline for connection

--- tcp/server.go
+++ tcp/server.go
@@ -12,12 +12,21 @@ func Run() {
 	for {
 		conn, _ := l.Accept()
 		go func(conn net.Conn) {
+			conn.SetDeadline(time.Now().Add(15 * time.Second))
+			conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+			conn.SetReadDeadline(time.Now().Add(10 * time.Second))
+
 			buf := make([]byte, 1024)
 			conn.Read(buf)
 			fmt.Println(string(buf))
 
 			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nHello, World!\r\n"))
 			time.Sleep(5 * time.Second)
+			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nHello, World!\r\n"))
+			time.Sleep(5 * time.Second)
+			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nHello, World!\r\n"))
+			time.Sleep(5 * time.Second)
+			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nHello, World!\r\n"))
 			conn.Close()
 		}(conn)
 	}
