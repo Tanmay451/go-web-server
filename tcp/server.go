93960319883ad4982ca18b3551cc80910c58ef99
Tanmay451 <tanmaypiyushkaushik451@gmail.com>
2024-04-10 21:40:54 +0530

    struct code for better reading

--- tcp/server.go
+++ tcp/server.go
@@ -6,28 +6,51 @@ import (
 	"time"
 )
 
-func Run() {
-	l, _ := net.Listen("tcp", ":8080")
+type Server struct {
+	addr string
+	ln   net.Listener
+}
 
-	for {
-		conn, _ := l.Accept()
-		go func(conn net.Conn) {
-			conn.SetDeadline(time.Now().Add(15 * time.Second))
-			conn.SetReadDeadline(time.Now().Add(5 * time.Second))
-			conn.SetReadDeadline(time.Now().Add(10 * time.Second))
-
-			buf := make([]byte, 1024)
-			conn.Read(buf)
-			fmt.Println(string(buf))
-
-			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nHello, World!\r\n"))
-			time.Sleep(5 * time.Second)
-			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nHello, World!\r\n"))
-			time.Sleep(5 * time.Second)
-			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nHello, World!\r\n"))
-			time.Sleep(5 * time.Second)
-			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nHello, World!\r\n"))
-			conn.Close()
-		}(conn)
+func NewServer(add string) *Server {
+	return &Server{
+		addr: add,
 	}
+}
+
+func (s *Server) Start() error {
+	ln, _ := net.Listen("tcp", s.addr)
+	s.ln = ln
+	s.accept()
+	return nil
+}
+
+func (s *Server) accept() error {
+	for {
+		conn, _ := s.ln.Accept()
+		go s.read(conn)
+	}
+}
+
+func (s *Server) read(conn net.Conn) {
+	conn.SetDeadline(time.Now().Add(15 * time.Second))
+	conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+	conn.SetReadDeadline(time.Now().Add(10 * time.Second))
+
+	buf := make([]byte, 1024)
+	conn.Read(buf)
+	fmt.Println(string(buf))
+
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nHello, World!\r\n"))
+	time.Sleep(5 * time.Second)
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nHello, World!\r\n"))
+	time.Sleep(5 * time.Second)
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nHello, World!\r\n"))
+	time.Sleep(5 * time.Second)
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nHello, World!\r\n"))
+	conn.Close()
+}
+
+func Run() {
+	server := NewServer(":3000")
+	server.Start()
 }
